6a838ab66cac7c3af4bb90220cb0719fc01c8b26
ordenygestion <119407619+ordenygestion@users.noreply.github.com>
2026-02-04 16:01:16 +0100

    Add screenCaptureDidChange method to ScreenshotDetector

--- src/ios/ScreenshotDetector.h
+++ src/ios/ScreenshotDetector.h
@@ -4,6 +4,9 @@
 
 - (void)pluginInitialize;
 - (void)userDidTakeScreenshot:(NSNotification *)notification;
+- (void)screenCaptureDidChange:(NSNotification *)notification;
 - (void)startDetection:(CDVInvokedUrlCommand*)command;
+
+@property (nonatomic) BOOL wasCapturing;
 
 @end
